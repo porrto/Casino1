38a11c4ae49f3bfd370c9e7d34048297a4d6f623
EnzoHeroguel <enzo.heroguel@isen-lille.fr>
2015-02-10 15:15:04 +0100

    commit

--- src/Blackjack/Main.java
+++ src/Blackjack/Main.java
@@ -1,37 +1,37 @@
 package Blackjack;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class Main {
 
-    private Vector main;
+    private ArrayList main;
 
     public Main() {
 
-        main = new Vector();
+        main = new ArrayList();
     }
 
     public void vider() {
 
-        main.removeAllElements();
+        main.clear();
     }
 
     public void ajouterCarte(Carte c) {
 
         if (c != null) {
-            main.addElement(c);
+            main.add(c);
         }
     }
 
     public void enleverCarte(Carte c) {
 
-        main.removeElement(c);
+        main.remove(c);
     }
 
     public void enleverCartePosition(int position) {
 
         if (position >= 0 && position < main.size()) {
-            main.removeElementAt(position);
+            main.remove(position);
         }
     }
 
@@ -43,7 +43,7 @@ public class Main {
     public Carte selectionnerCarte(int position) {
 
         if (position >= 0 && position < main.size()) {
-            return (Carte) main.elementAt(position);
+            return (Carte) main.get(position);
         } else {
             return null;
         }
@@ -51,40 +51,40 @@ public class Main {
 
     public void triParSuites() {
 
-        Vector nouvelleMain = new Vector();
+        ArrayList nouvelleMain = new ArrayList();
         while (main.size() > 0) {
             int pos = 0;  // Position of minimal card.
-            Carte c = (Carte) main.elementAt(0);  // Minumal card.
+            Carte c = (Carte) main.get(0);  // Minumal card.
             for (int i = 1; i < main.size(); i++) {
-                Carte c1 = (Carte) main.elementAt(i);
+                Carte c1 = (Carte) main.get(i);
                 if (c1.getSuite() < c.getSuite()
                         || (c1.getSuite() == c.getSuite() && c1.getValeur() < c.getValeur())) {
                     pos = i;
                     c = c1;
                 }
             }
-            main.removeElementAt(pos);
-            nouvelleMain.addElement(c);
+            main.remove(pos);
+            nouvelleMain.add(c);
         }
         main = nouvelleMain;
     }
 
     public void triParValeur() {
 
-        Vector nouvelleMain = new Vector();
+        ArrayList nouvelleMain = new ArrayList();
         while (main.size() > 0) {
             int pos = 0;  // Position of minimal card.
-            Carte c = (Carte) main.elementAt(0);  // Minumal card.
+            Carte c = (Carte) main.get(0);  // Minumal card.
             for (int i = 1; i < main.size(); i++) {
-                Carte c1 = (Carte) main.elementAt(i);
+                Carte c1 = (Carte) main.get(i);
                 if (c1.getValeur() < c.getValeur()
                         || (c1.getValeur() == c.getValeur() && c1.getSuite() < c.getSuite())) {
                     pos = i;
                     c = c1;
                 }
             }
-            main.removeElementAt(pos);
-            nouvelleMain.addElement(c);
+            main.remove(pos);
+            nouvelleMain.add(c);
         }
         main = nouvelleMain;
     }
